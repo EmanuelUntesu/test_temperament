dbccae20ac982731e783689e9a4bbe630d15e097
EmanuelUntesu <emanuel@untesu.ro>
2026-02-17 20:08:17 +0100

    Pregătire pentru deployment pe Render

--- test_temperament/settings.py
+++ test_temperament/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3+&=ccik4#pp2%^19u3e72v)^60!g^13j8e4+w*=d#an-#v-qn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/quizz/rezultate/'
 
 LOGOUT_REDIRECT_URL = '/accounts/login/'
